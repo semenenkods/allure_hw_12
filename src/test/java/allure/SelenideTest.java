45027f563fc4a4316da329178a74a4dbf0e3a18d
semenenkods <semenenkods@gmail.com>
2024-03-03 19:55:50 -0700

    added allure report

--- src/test/java/allure/SelenideTest.java
+++ src/test/java/allure/SelenideTest.java
@@ -1,17 +1,30 @@
 package allure;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.By.linkText;
 
 public class SelenideTest {
     @Test
     @DisplayName("Search of issue in the repository")
     public void testIssueSearch() {
+
+        SelenideLogger.addListener("allure", new AllureSelenide());
         open("https://github.com");
         $(".search-input").click();
+        $("#query-builder-test").sendKeys("semenenkods/allure_hw_12");
+        $("#query-builder-test").pressEnter();
+        $(linkText("semenenkods/allure_hw_12")).click();
+        $("#issues-tab").click();
+        $(withText("#1")).should(Condition.exist);
+
 
     }
 }
